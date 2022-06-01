56c68895842bf04b27ad0c02f00971cabf61f835
edwinaikins <edwinaikins@gmail.com>
2022-06-01 03:28:38 +0000

    lopps and functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 for i in reversed(range(97, 123)):
-	print(f"{i if (i % 2 == 0) else (i -32):c}", end = '')
+    print(f"{i if (i % 2 == 0) else (i - 32):c}", end='')
