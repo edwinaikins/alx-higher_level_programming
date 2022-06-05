5fad5fd11ac35562e5a3e48d220baea93f45c603
edwinaikins <edwinaikins@gmail.com>
2022-06-05 14:05:59 +0000

    Data Structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = my_string[:]
     for index in range(len(my_string)):
         if my_string[index] == 'c' or my_string[index] == 'C':
-            new_string = new_string[:index] + my_string[(index+1):]
+            new_string = new_string[:index] + my_string[(index + 1):]
     return new_string
