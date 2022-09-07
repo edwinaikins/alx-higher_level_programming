0a4653aa551b7816b48e0035b03919452acd6667
edwinaikins <edwinaikins@gmail.com>
2022-09-07 02:19:52 +0000

    Javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let msg = 'C is fun';
-let msg1 = 'Python is cool';
-let msg2 = 'JavaScript is amazing';
-console.log('%s\n%s\n%s',msg,msg1,msg2);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
